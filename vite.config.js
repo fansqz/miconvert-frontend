155f08f6b00e75f984bc231f65b0aae64f57c684
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-19 12:02:52 +0800

    添加请求拦截器

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        targeet: 'http://localhost:8080',
+        changeOrigin: true,
+        //拦截请请求路径/api/
+        rewrite: (path) => path.replace(/^\/api/,''),
+      }
+    }
+  }
 });
