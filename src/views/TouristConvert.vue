53115cb7d317063e9578468f0b07e32349314c2a
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-26 00:00:14 +0800

    上传文件时添加提示

--- src/views/TouristConvert.vue
+++ src/views/TouristConvert.vue
@@ -3,7 +3,6 @@
   <el-upload
     class="upload-demo"
     drag
-    accept=".pdf,.doc,.docx,.png,.jpg"
     :before-upload="beforeUpload"
     :http-request="convertFile"
     :on-remove="handleRemove"
@@ -11,16 +10,17 @@
   >
     <el-icon class="el-icon--upload"><upload-filled /></el-icon>
     <div class="el-upload__text">
-        将文件拖到此处，<em>或点击上传</em>
+        将文件拖到此处，<em>或点击上传</em><br/>
+        仅支持{{inFormatsString}}转换为{{outFormat}}
     </div>
     <!--下拉框-->
     <template #tip>
       <el-form-item label="目标类型：">
-              <el-select v-model="outFormat" @click="getOutFormats">
-                <el-option v-for="(item,index) in outFormats"
-                :key="index" :label="item" :value="item">
-                </el-option>
-              </el-select>
+        <el-select v-model="outFormat" @click="getOutFormats" @change="getInFormatsByOutFormats">
+          <el-option v-for="(item,index) in outFormats"
+          :key="index" :label="item" :value="item">
+          </el-option>
+        </el-select>
     </el-form-item>
     </template>
   </el-upload><br/>
@@ -51,9 +51,13 @@ import convert from '../service/api/convert';
 // 文件列表
 const fileList = ref([]);
 // 选择的格式
-const outFormat = ref();
+const outFormat = ref('pdf');
 // 可选的格式
 const outFormats = ref([]);
+// 可输入格式
+const inFormats = ref([]);
+
+const inFormatsString = ref();
 
 // 添加文件并校验
 const beforeUpload = (file) => {
@@ -71,7 +75,7 @@ const convertFile = async (params) => {
   // 验证数据是否合格
   const fd = new FormData();
   fd.append('file', params.file);
-  fd.append('outFormat', 'pdf');
+  fd.append('outFormat', outFormat.value);
   // 添加文件并记录位置
   const index = fileList.value.length;
   fileList.value.push({
@@ -93,6 +97,16 @@ const getOutFormats = async () => {
   outFormats.value = res.data;
 };
 
+// 根据目标格式获取输入格式
+const getInFormatsByOutFormats = async () => {
+  const res = await convert.listAllInFormatByOutFormat({ outFormat: outFormat.value });
+  // 设置各种属性
+  inFormats.value = res.data;
+  inFormatsString.value = inFormats.value.toString();
+};
+
+// 进入页面时就获取pfd支持的inFormat
+getInFormatsByOutFormats();
 </script>
 
 <style>
@@ -102,4 +116,5 @@ const getOutFormats = async () => {
   display: flex;
   align-items: center;
 }
+
 </style>
